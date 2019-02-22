ac9cf7345456e6495f93ba801678c472d8998fb9
Jefferson Duong <jeffdeeeee21@gmail.com>
2019-02-22 09:17:45 -0500

    changed localhosts

--- mern-calendar-app/src/components/CreateEvent/CreateEvent.js
+++ mern-calendar-app/src/components/CreateEvent/CreateEvent.js
@@ -42,7 +42,7 @@ class CreateEvent extends Component {
 
     handleSubmit(e) {
         // e.preventDefault()
-        axios.post('http://localhost:3001/events/new', this.state )
+        axios.post('https://dashboard.heroku.com/apps/lettuce-meat-api/events/new', this.state )
             .then(() => {
                 // console.log(this.state)
                 // console.log('hello')
